b958c550d44d7bf21083eaae3826a6ca565c0d7a
kuhcretsen1 <148473755+kuhcretsen1@users.noreply.github.com>
2024-09-25 15:30:27 +0300

    test

---
+++ lab3/src/components/SearchInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SearchInput = ({searchQuery,handleSearchChange}) => {
+  return (
+<input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+  )
+}
+
+export default SearchInput
